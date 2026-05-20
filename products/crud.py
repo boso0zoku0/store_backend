d8a836035686db5a804a52f94876c6a40c23cc0c
BOSOZOKU <matveygrishayev@mail.ru>
2026-05-20 19:54:06 +0300

    test redis

--- products/crud.py
+++ products/crud.py
@@ -182,6 +182,11 @@ async def search_product(short_name: str, session: AsyncSession):
 
 
 async def find_product_by_filters(filters: Filters, session: AsyncSession):
+    cache_key = "products:filters"
+    cache = await redis_client.get(cache_key)
+    if cache:
+        return json.loads(cache)
+    
     stmt = select(Products)
     print(f"Фильтры: {filters}")
     conditions = []
@@ -213,9 +218,14 @@ async def find_product_by_filters(filters: Filters, session: AsyncSession):
                 conditions.append(Products.filters["volume"] == filters.volume)
             stmt = stmt.where(and_(*conditions))
             result = await session.execute(stmt)
-            print(f"УСЛОВИЯ: {conditions}")
-            print(f"ВЫВОД: {result.scalars().all()}")
-            return result.scalars().all()
+            
+            res = result.scalars().all()
+            await redis_client.set(
+                cache_key,
+                json.dumps(res),
+                ex=60,
+            )
+            return res
     else:
         print(f"Второстепенный поиск")
         print(f"price: {filters.priceRange}")
